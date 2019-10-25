2dd3737ff5bbee8d3d15dcc815933a90fad7af45
nayeemahmed24 <42116405+nayeemahmed24@users.noreply.github.com>
2019-10-25 15:55:13 +0600

    bug fixed

--- Daraz/src/main/java/com/daraz/DarazApplication.java
+++ Daraz/src/main/java/com/daraz/DarazApplication.java
@@ -1,12 +1,15 @@
-package com.daraz;
+package com.daraz.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.daraz.obj.FileStorageProperties;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties({
     FileStorageProperties.class
 })
